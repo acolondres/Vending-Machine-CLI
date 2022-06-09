ff85f2363af7f98b597682794c5728c1da7de54b
Cesar Andino <inarikaze@gmail.com>
2022-06-09 10:53:38 -0400

    Pushing CG help changes

--- src/main/java/com/techelevator/InventoryClass.java
+++ src/main/java/com/techelevator/InventoryClass.java
@@ -9,28 +9,24 @@ import java.util.Scanner;
 public class InventoryClass {
 
 
-
-
-
-
-
-
-    List<ProductClass> itemName = new ArrayList<>();
-    try(Scanner fileReader = new Scanner(new File("C:\\Users\\Student\\workspace\\nlr-8-module-1-capstone-orange-team-8\\vendingmachine.csv")))
+    public void itemList()
 
     {
-        while (fileReader.hasNextLine()) {
-            String currentLine = fileReader.nextLine();
-            String[] productList = currentLine.split("\|");
-            String slot = productList[0];
-            String name = productList[1];
-            String price = productList[2];
-            String type = productList[3];
-            itemName.add(new ProductClass(slot, name, price, type));
-        }
+        List<ProductClass> itemName = new ArrayList<>();
+        try (Scanner fileReader = new Scanner(new File("C:\\Users\\Student\\workspace\\nlr-8-module-1-capstone-orange-team-8\\vendingmachine.csv"))) {
+            while (fileReader.hasNextLine()) {
+                String currentLine = fileReader.nextLine();
+                String[] productList = currentLine.split("\\|");
+                String slot = productList[0];
+                String name = productList[1];
+                String price = productList[2];
+                String type = productList[3];
+                itemName.add(new ProductClass(slot, name, price, type));
+            }
 
-    } catch(FileNotFoundException ex) {
-        System.out.println("Item list not available");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Item list not available");
+        }
     }
 }
 
